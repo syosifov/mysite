b393ae31771d484e5e217f6c0d261353ab6fd77e
Strashimir Yosifov <syosifov@gmail.com>
2023-05-13 20:44:18 +0300

    v2

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&!g1m_4c=xjd=ur5l)2!gdm1^lc@3r0h7ch!)h!@t7sbxr^mvs'
+# SECRET_KEY = 'django-insecure-&!g1m_4c=xjd=ur5l)2!gdm1^lc@3r0h7ch!)h!@t7sbxr^mvs'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-&!g1m_4c=xjd=ur5l)2!gdm1^lc@3r0h7ch!)h!@t7sbxr^mvs')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:    
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
@@ -74,12 +82,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+    'default': dj_database_url.config(        
+        default='postgresql://postgres:postgres@localhost:5432/mysite',        
+        conn_max_age=600    
+    )}
 
 
 # Password validation
